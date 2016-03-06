c7ebbc7a4000b9d4df0c4883a7b8d066a5acf70c
Soulfish McGoo <ryanjbabcock@gmail.com>
2016-03-05 20:09:02 -0800

    S3 Uploader with ICC videostream
    Success! I can stream videos to Amazon S3!

--- AmazonS3VideoStreaming/src/InstantCloudCameraRunner.java
+++ AmazonS3VideoStreaming/src/InstantCloudCameraRunner.java
@@ -3,7 +3,7 @@
 import org.bytedeco.javacv.*;
 //import org.bytedeco.javacpp.*;
 //import org.bytedeco.javacpp.indexer.*;
-import static org.bytedeco.javacpp.opencv_core.*;
+//import static org.bytedeco.javacpp.opencv_core.*;
 //import static org.bytedeco.javacpp.opencv_imgproc.*;
 //import static org.bytedeco.javacpp.opencv_calib3d.*;
 //import static org.bytedeco.javacpp.opencv_objdetect.*;
@@ -11,30 +11,28 @@ import static org.bytedeco.javacpp.opencv_core.*;
 //Taken from: https://github.com/bytedeco/javacv#sample-usage
 
 public class InstantCloudCameraRunner implements Runnable {
-	//final int INTERVAL=1000;///you may use interval
-	IplImage image;
-	CanvasFrame canvas = new CanvasFrame("Instant Cloud Camera");
+	private final String PREFIX = "myvideo";
+	private final static String BUCKETNAME = "icc-videostream-00";
+	private final static int MAX_SEGMENTS = 10;
+	private CanvasFrame canvas = new CanvasFrame("Instant Cloud Camera");
+	private static S3Uploader s3;
+	private static SharedQueue<String> que;
+	
+	//NEED BETTER CLOSING
 	public InstantCloudCameraRunner() {
 		canvas.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
 	}
 	@Override
 	public void run() {
-		String outputFileName = "myvideo.flv";
+		int segmentNum = 0;
+		String outputFileName = PREFIX + segmentNum + ".flv";
 		FrameGrabber grabber = null;
 		FFmpegFrameRecorder recorder = null;
 //		https://stackoverflow.com/questions/28494648/video-compression-using-javacv
 		try{
 			grabber = FrameGrabber.createDefault(0); // 1 for next camera
+//			grabber.setFrameRate(720);
 			grabber.start();
-			recorder = new FFmpegFrameRecorder(outputFileName,
-					grabber.getImageWidth(), grabber.getImageHeight(),
-					grabber.getAudioChannels());
-			recorder.start();
-			recorder.setFrameRate(grabber.getFrameRate());
-			recorder.setSampleRate(grabber.getSampleRate());
-	        recorder.setFrameRate(30);
-	        recorder.setVideoBitrate(10 * 1024 * 1024);
-			recorder.setFormat("flv");
 		}
 		catch(Exception e){
 			System.err.println(e + ": Unable to record video...");
@@ -42,20 +40,46 @@ public class InstantCloudCameraRunner implements Runnable {
 		}
 		try {
 			Frame img;
+			long start = 0;
 			while (true) {
-				img = grabber.grab();
-				if (img != null) {
+				if(recorder == null){
+					outputFileName = PREFIX + (++segmentNum) + ".flv";
+					recorder = new FFmpegFrameRecorder(outputFileName,
+							grabber.getImageWidth(), grabber.getImageHeight(),
+							grabber.getAudioChannels());
+					recorder.start();
+					recorder.setFrameRate(grabber.getFrameRate());
+					recorder.setSampleRate(grabber.getSampleRate());
+//			        recorder.setFrameRate(10);
+//			        recorder.setVideoBitrate(10 * 1024 * 1024);
+					recorder.setFormat("flv");
+					start = System.currentTimeMillis();
+				}
+				if ((img = grabber.grab()) != null) {
 					// show image on window
 					canvas.showImage(img);
 					recorder.record(img);
 				}
-				//Thread.sleep(INTERVAL);
+				//check length of video
+				if(System.currentTimeMillis() - start >= 8000){
+					try{
+						recorder.stop();
+					} catch(Exception e){
+						System.err.println(e + ": Unable to save video!");
+						e.printStackTrace();
+					}
+					recorder = null;
+					segmentNum = segmentNum % MAX_SEGMENTS;
+					que.enqueue(outputFileName);
+				}
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		try{
 			recorder.stop();
+			s3.end();
+			s3.interrupt();
 		} catch(Exception e){
 			System.err.println(e + ": Unable to save video!");
 			e.printStackTrace();
@@ -63,8 +87,11 @@ public class InstantCloudCameraRunner implements Runnable {
 	}
 
 	public static void main(String[] args) {
-		InstantCloudCameraRunner gs = new InstantCloudCameraRunner();
-		Thread th = new Thread(gs);
+		InstantCloudCameraRunner iccr = new InstantCloudCameraRunner();
+		que = new SharedQueue<>(MAX_SEGMENTS);
+		s3 = new S3Uploader(BUCKETNAME, que);
+		Thread th = new Thread(iccr);
+		s3.start();
 		th.start();
 	}
 }
