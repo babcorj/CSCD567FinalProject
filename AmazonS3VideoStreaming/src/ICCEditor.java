ba49cfab14f2340d9d941731f378800d97acb246
Soulfish McGoo <ryanjbabcock@gmail.com>
2016-03-12 12:43:12 -0800

    ignore
    ...

--- AmazonS3VideoStreaming/src/ICCEditor.java
+++ AmazonS3VideoStreaming/src/ICCEditor.java
@@ -1,3 +1,4 @@
+import org.bytedeco.javacpp.BytePointer;
 import org.bytedeco.javacpp.opencv_core.IplImage;
 import org.bytedeco.javacv.FFmpegFrameRecorder;
 import org.bytedeco.javacv.OpenCVFrameConverter;
@@ -29,9 +30,30 @@ public class ICCEditor {
 				ipimg.height()*3/ratio, ipimg.arrayDepth(), 0);
 		int oldlength = ipimg.width() * ipimg.height() * 3;
 		int newlength = oldlength/ratio;
+		int average = 0;
 		for(int i = 0; i < newlength; i++){
-			newImage.imageData(i) = 0;
+			if(i ==2 )
+			average = getAverage(ipimg, i, ratio);
+			newImage.imageData().put(i, (byte) average);
 		}
+	}
+	
+	//Used to get average pixel value from x amount of pixels
+	private int getAverage(IplImage ip, int pos, int ratio){
+		int sum = 0;
+		int coverage = 0;
+		int widthstep = ip.width()*3;
+		coverage = (int)log2(ratio);
+		for(int i = 0; i < coverage; i += widthstep){
+			for(int j = i; j < coverage; j++){
+				sum += ip.imageData().get(j);
+			}
+		}
+		return sum;
+	}
+	
+	public double log2(double num){
+		return Math.log(num)/Math.log(2.0d);
 	}
 	
 	/*
